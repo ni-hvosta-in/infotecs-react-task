675d3e8c490605758dfeed807a1059675023fded
Alexandr <alpinefuchs@gmail.com>
2026-02-16 23:30:05 +0300

    fix: settings for phone

--- src/widgets/UserListWidget/UserListWidget.style.ts
+++ src/widgets/UserListWidget/UserListWidget.style.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     max-height: 80vh;
     overflow-y: auto;
+
+    @media (max-width: 768px) {
+        max-height: 60vh;
+    }
 `;

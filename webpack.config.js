7b92a5058b1eac6b9dbe5f9cb523f4b075ae6ca8
Alexandr <alpinefuchs@gmail.com>
2026-02-16 22:09:01 +0300

    feature: try webpack settings to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,59 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public")
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development";
+
+  return {
+    mode: isDev ? "development" : "production",
+
+    entry: "./src/index.tsx",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/", 
+      clean: true
     },
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    hot: true,
-    watchFiles: ["src/**/*"]
-  }
+
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      alias: {
+        "@": path.resolve(__dirname, "src")
+      }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      })
+    ],
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public")
+      },
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      hot: true,
+      watchFiles: ["src/**/*"]
+    },
+
+    devtool: isDev ? "inline-source-map" : false
+  };
 };
